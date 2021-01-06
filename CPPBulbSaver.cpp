16fd6867230ecd3d82ec76431bdc708e645486af
Robert Wood Jr <rdrwood@gmail.com>
2021-01-05 21:27:12 -0800

    comments

--- CPPBulbSaver.cpp
+++ CPPBulbSaver.cpp
@@ -4,13 +4,23 @@
 #include <iostream>
 #include<Windows.h>
 
-
+/// <summary>
+///  This will print out the header to the program
+/// </summary>
 void printHeader() {
     std::cout << std::endl << "This console app is to determine cost savings to switch to LED bulbs" << std::endl;
     std::cout << "====================================================================" << std::endl;
     // (note: you may have to contact power company)
 }
 
+/// <summary>
+/// this will print out the results of the calculations
+/// </summary>
+/// <param name="monthlyCost"> the monthly cost currently</param>
+/// <param name="afterMonthlyCost">the monthly cost after changing to led</param>
+/// <param name="payOffCost"> how much to pay off bulbs</param>
+/// <param name="monthlyKWH">how many KWH monthly currently used</param>
+/// <param name="afterMonthlyKWH">how many KWH monthly used after led change</param>
 void printResults( double monthlyCost, double afterMonthlyCost,double payOffCost, double monthlyKWH, double afterMonthlyKWH) {
     
     double howLong = payOffCost / (monthlyCost - afterMonthlyCost);
@@ -32,12 +42,19 @@ void printResults( double monthlyCost, double afterMonthlyCost,double payOffCost
     std::cout << "Savings " << monthlyKWH - afterMonthlyKWH << " KWH" << std::endl;
 }
 
+/// <summary>
+/// This function will show the message calculating then wait before contnuing to display results
+/// </summary>
 void calculating() {
     std::cout << std::endl << std::endl;
     std::cout << "Calculating .... " << std::endl;
     Sleep(1000);
 }
 
+/// <summary>
+/// this function will ask the user their power rate 
+/// </summary>
+/// <returns>will return the powerRate</returns>
 double powerRate() {
     std::cout << std::endl << std::endl << std::endl;
     std::cout << "First, what is your power rate? > " << std::flush;
@@ -46,6 +63,10 @@ double powerRate() {
     return powerRate;
 }
 
+/// <summary>
+/// this function will ask the user how many bulbs
+/// </summary>
+/// <returns>how many bulbs entered by the user</returns>
 int howManyBulbs() {
     std::cout << "How many light bulbs? > " << std::flush;
     int howManyBulbs;
@@ -53,20 +74,30 @@ int howManyBulbs() {
     return howManyBulbs;
 }
 
+/// <summary>
+/// this function will ask the user their current bulbs wattages
+/// </summary>
+/// <returns>current bulb wattage</returns>
 int howManyWattsCurrently() {
     std::cout << "How many watts are current bulbs? > " << std::flush;
     int currentWatts;
     std::cin >> currentWatts; 
     return currentWatts;
 }
-
+/// <summary>
+/// this function will ask the user their led bulbs wattage
+/// </summary>
+/// <returns>the new led bulb wattage</returns>
 int howManyWattsNewBulbs() {
     std::cout << "How many watts will the new bulbs be? > " << std::flush;
     int replacementWatts;
     std::cin >> replacementWatts;
     return replacementWatts;
 }
-
+/// <summary>
+/// this function will ask the user how many hours their bulbs are used
+/// </summary>
+/// <returns>how many hours the bulbs are used</returns>
 double howManyHoursUsed() {
     std::cout << "How many hours used? > " << std::flush;
     double howManyHours;
@@ -74,6 +105,15 @@ double howManyHoursUsed() {
     return howManyHours;
 }
 
+/// <summary>
+/// calculates the results of the led bulb conversion
+/// </summary>
+/// <param name="powerCostRate">rate of power cost</param>
+/// <param name="howManyBulbsUsed">how many bulbs are used</param>
+/// <param name="howManyHours">how many hours bulbs are used</param>
+/// <param name="currentWatts">current bulbs wattage</param>
+/// <param name="replacementWatts">led bulbs wattage</param>
+/// <param name="bulbCosts"> the cost of the bulbs</param>
 void calculate(double powerCostRate, int howManyBulbsUsed, double howManyHours, int currentWatts, int replacementWatts, double bulbCosts) {
     double currCost;
     double ledApprox;
@@ -96,6 +136,10 @@ void calculate(double powerCostRate, int howManyBulbsUsed, double howManyHours, 
     printResults( monthlyCost, afterMonthlyCost, payOffCost, monthlyKWH, afterMonthlyKWH);
 }
 
+/// <summary>
+/// this function will ask the user how much led bulb will cost
+/// </summary>
+/// <returns>cost of led bulb</returns>
 double howMuchNewBulbCost() {
     std::cout << std::endl << std::endl << std::endl;
     std::cout << "How much will the led bulbs cost? > " << std::flush;
@@ -103,6 +147,10 @@ double howMuchNewBulbCost() {
     std::cin >> ledBulbCost;
     return ledBulbCost;
 }
+/// <summary>
+/// this will take user inputs and determine the cost savings of converting to led bulbs
+/// </summary>
+/// <returns></returns>
 int main()
 {
     // Prints opening header 
